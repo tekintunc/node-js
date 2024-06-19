5e26357a86d6ad78d03414f9497fa0f98feaf4fa
tekintunc <115110134+tekintunc@users.noreply.github.com>
2024-06-19 19:32:06 +0200

    l3

--- lesson3/fs.js
+++ lesson3/fs.js
@@ -1,10 +1,25 @@
 const fs = require("fs")
 
-fs.readFile("file.txt", "utf8", (err ,data)=>{
-    if(err){
-        console.log(err)
-    }else {
-        console.log(data)
-    }
+// fs.readFile("file.txt", "utf8", (err ,data)=>{
+//     if(err){
+//         console.log(err)
+//     }else {
+//         console.log(data)
+//     }
 
-} )
+// } )
+// const newContent ="New Content was added!!!"
+// fs.writeFile("My-file.txt", newContent,"utf-8", (err) => {
+//     if (err) {
+//         console.log(err)
+//     } else {
+//         console.log("test is writen.")
+//     }
+// }) 
+fs.unlink("file.txt",(err) => {
+    if(err) {
+        console.log("Hata oluştu")
+    } else {
+        console.log("Dosya silindi")
+    }
+}) 
---
+++ lesson3/path.js
@@ -0,0 +1,4 @@
+const path = require("path")
+
+console.log(path.basename("\Users\tekin\OneDrive\Masaüstü\Backend\nodejs\lesson3"))
+console.log(path.extname(path))
