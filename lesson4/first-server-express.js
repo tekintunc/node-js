657836417c6650eee8f895cdc61295c6862d036c
tekintunc <115110134+tekintunc@users.noreply.github.com>
2024-06-22 17:25:04 +0200

    L4 get

--- lesson4/first-server-express.js
+++ lesson4/first-server-express.js
@@ -1,17 +1,17 @@
-const express = require("express")
-const app = express()
+// const express = require("express")
+// const app = express()
 
-const hostname = "127.0.0.1"
-const port = 3000
+// const hostname = "127.0.0.1"
+// const port = 3000
 
-app.get("/", (req,res)=>{
-    res.status(200).send("My First express server!!!!!")
-})
+// app.get("/", (req,res)=>{
+//     res.status(200).send("My First express server!!!!!")
+// })
 
 
-app.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-})
+// app.listen(port, hostname, () => {
+//     console.log(`Server running at http://${hostname}:${port}/`);
+// })
 
 
 
--- lesson4/server-with-different-url-express.js
+++ lesson4/server-with-different-url-express.js
@@ -4,14 +4,33 @@ const app = express()
 const hostname = "127.0.0.1"
 const port = 3000
 
-app.get("/text", (req,res)=>{
-    res.status(200).send("My First express server!!!!!")
+const users = [
+    {id :1, name: "Mehmet"},
+    {id :2, name: "Ahmet"},
+    {id :3, name: "Ali"},
+    {id :4, name: "Dali"},
+    {id :5, name: "Mali"},
+]
+
+app.get("/users", (req,res)=>{
+    res.status(200).send(users)
 })
 
-app.get("/json", (req,res)=>{
-    res.status(200).send([{id: 1, name: "Tekin"}])
+app.get("/users/:id", (req,res)=>{
+    const userId = Number(req.params.id)
+    const user = users.find((user => user.id === userId))   
+
+        if (user) {   
+            res.status(200).send(user);
+        } else {        
+            
+            res.status(404).send({ message:'User not found!!!'});
+        }    
 })
 
+app.use((req, res) => {
+    res.status(404).send({ message:' Not found!!!'});
+})
 
 app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
